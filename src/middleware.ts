abc92adb449340b6b0d3ca1ccba9444feed1adce
Kasia <katarzynamakarowicz@gmail.com>
2023-06-15 11:37:02 +0200

    blah blah

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ export default authMiddleware({
   afterAuth(auth, req, evt) {
     // handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return redirectToSignIn({ returnBackUrl: req.url });
     }
   },
