a35d2d0f302cd2dab4b073e125728dd2855cdfd3
Evan Seaward <seaward.evan@gmail.com>
2023-07-07 15:41:08 +0200

    remove other langs for now

--- src/i18n/pl/index.ts
+++ src/i18n/pl/index.ts
@@ -3,18 +3,22 @@ import type { Translation } from "../i18n-types";
 const pl: Translation = {
   title: {
     hashtags: "Hasztagi",
-    madeSimple: "Hasztagi",
-    forCreators: "Hasztagi",
+    madeSimple: "proste jak nigdy dotąd",
+    forCreators: "dla twórców",
   },
-  tryForFree: "Hasztagi",
-  whileManyMightBe: "Hasztagi",
-  unleashTheMagic: "Hasztagi",
-  generateInstantly: "Hasztagi",
-  hashtagGeneration: "Hasztagi",
-  boostReach: "Hasztagi",
-  getStarted: "Hasztagi",
-  forCreatorsByCreators: "Hasztagi",
-  today: "Hasztagi",
+  tryForFree: "Wypróbuj za darmo",
+  whileManyMightBe:
+    "Podczas gdy wiele generatorów hasztagów może być skomplikowanych w użyciu, my stawiamy na prostotę, nie rezygnując z precyzji. Pożegnaj się z złożonymi narzędziami i nie martw się o wybór hasztagów.",
+  unleashTheMagic: "Uwolnij moc hasztagów!",
+  generateInstantly: "Generuj natychmiast",
+  hashtagGeneration: "Generowanie hasztagów",
+  boostReach: `Zwiększ swoje zasięgi dzięki potężnym hasztagom! Odblokuj
+    potencjał generowania hasztagów i wzmocnij swoją strategię
+    na mediach społecznościowych już dziś.`,
+  getStarted: "Rozpocznij",
+  forCreatorsByCreators: "Dla twórców, przez twórców",
+  today: "dzisiaj",
 };
+
 
 export default pl;
